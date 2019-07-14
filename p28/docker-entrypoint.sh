b87a46e4f1b69f3b9f19743cc9f6f7441bf3754c
Tekki <tekki@tekki.ch>
2019-07-14 14:59:20 +0200

    Update to version 0.06.

--- p28/docker-entrypoint.sh
+++ p28/docker-entrypoint.sh
@@ -5,6 +5,11 @@
 # do not edit it directly
 #
 
+if [ $DEBIAN_PACKAGES ]; then
+  apt-get update
+  apt-get install -y $DEBIAN_PACKAGES
+fi
+
 if [ $INSTALLDEPS ] && [ -f cpanfile ]; then
   cpanm --installdeps .
 fi
