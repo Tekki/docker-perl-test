1286fe254b611fc811cc4e13b2028a3a96630340
Tekki <tekki@tekki.ch>
2021-05-22 21:03:53 +0200

    Add files for Perl 5.34.

---
+++ p34/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# this file is generated via docker-builder/generate.pl
+#
+# do not edit it directly
+#
+
+if [ $DEBIAN_PACKAGES ]; then
+  apt-get update
+  apt-get install -y $DEBIAN_PACKAGES
+fi
+
+if [ $INSTALLDEPS ] && [ -f cpanfile ]; then
+  cpanm --installdeps --with-recommends .
+fi
+
+exec "$@"
